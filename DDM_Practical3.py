09a56cf7acd27459ccd01c2a71261572a6115630
Unknown <joris@lambooy.nl>
2018-06-04 23:15:15 +0200

    Intersection

--- DDM_Practical3.py
+++ DDM_Practical3.py
@@ -13,6 +13,7 @@ import ddm
 import bpy
 import numpy
 import math
+import mathutils
 
 # To view the printed output toggle the system console in "Window -> Toggle System Console"
 
@@ -56,11 +57,11 @@ def De_Casteljau(A, n, s):
                 for j in range(len(u_points) - 1):
                     u_points[j] = (u_points[j][0] + u * (u_points[j+1][0] - u_points[j][0]), u_points[j][1] + u * (u_points[j+1][1] - u_points[j][1]), u_points[j][2] + u * (u_points[j+1][2] - u_points[j][2]))
                 u_points.pop()
-            print("Replacing point at index " + str((i + 1) * n + x))
-            print("Replacing with " + str(u_points[0]))
+            #print("Replacing point at index " + str((i + 1) * n + x))
+            #print("Replacing with " + str(u_points[0]))
             new_mesh[(i + 1) * n + x] = u_points[0]
-        print("Replacing point at index " + str((n-1) * (s+1) * n + x))
-        print("Replacing with " + str(u_points[len(u_points) - 1]))
+        #print("Replacing point at index " + str((n-1) * (s+1) * n + x))
+        #print("Replacing with " + str(u_points[len(u_points) - 1]))
         new_mesh[(n-1) * (s+1) * n + x] = points.pop()
     
     #print(new_mesh)
@@ -99,6 +100,73 @@ def control_mesh(n, length):
     return array
     
 def line_intersect(A, n, p1, p2, e):
+    p1x = p1[0] + 10 * (p1[0] - p2[0])
+    p1y = p1[1] + 10 * (p1[1] - p2[1])
+    p1z = p1[2] + 10 * (p1[2] - p2[2])
+    p2x = p2[0] + 10 * (p2[0] - p1[0])
+    p2y = p2[1] + 10 * (p2[1] - p1[1])
+    p2z = p2[2] + 10 * (p2[2] - p1[2])
+    p1 = (p1x, p1y, p1z)
+    p2 = (p2x, p2y, p2z)
+    def distanceSQR(po1, po2):
+        (x1, y1, z1) = po1
+        (x2, y2, z2) = po2
+        return (x1 * x2 + y1 * y2 + z1 * z2)
+    def get_point(ax, ay):
+        return A[ax + ay * n]
+        return (a1 * b1 + a3 * b3)
+    def intersect(pA, pn, pe, minX, maxX, minY, maxY):
+        for x in range(minX, maxX):
+            for y in range(minY, maxY):
+                (a, b) = (get_point(x, y), get_point(x + 1, y))
+                a2 = (a[1], a[2])
+                b2 = (b[1], b[2])
+                p12 = (p1[1], p1[2])
+                p22 = (p2[1], p2[2])
+                intPoint = mathutils.geometry.intersect_line_line_2d(a2, b2, p12, p22)
+                # check intersection and bounding box
+                if intPoint != None:
+                    # intersection at this y-curve, so check corresponding x-curve
+                    for xx in range(minX, maxX):
+                        (a, b) = (get_point(xx, y), get_point(xx, y + 1))
+                        a2 = (a[0], a[2])
+                        b2 = (b[0], b[2])
+                        p12 = (p1[0], p1[2])
+                        p22 = (p2[0], p2[2])
+                        intPoint2 = mathutils.geometry.intersect_line_line_2d(a2, b2, p12, p22)
+                        if intPoint2 != None:
+                            # intersection
+                            iPoint = (intPoint2[0], intPoint[0], intPoint[1])
+                            print ("intersect at ca.", iPoint, intPoint2[1])
+                            return iPoint
+        return None
+    
+    length = get_point(n - 1, n - 1)[0]
+    minX = 0
+    maxX = n - 1
+    minY = 0
+    maxY = n - 1
+    for i in range(10):
+        iPoint = intersect(A, n, e, minX, maxX, minY, maxY)
+        print("e", length / n)
+        if iPoint != None:
+           # currE = min(distanceSQR(iPoint, 
+            if length / n < e:
+                # precise enough
+                return True
+            # might intersect, so subdiv and try again
+            A = De_Casteljau(A, n, 1)
+            old_n = n
+            n = subdivisions(n, 1)
+            
+            minX = math.floor(iPoint[0] / old_n * n) - 3
+            maxX = math.floor(iPoint[0] / old_n * n) + 4
+            minY = math.floor(iPoint[1] / old_n * n) - 3
+            maxY = math.floor(iPoint[1] / old_n * n) + 4
+        else:
+            #guaranteed no intersection, so return False
+            return False
+    # give up, and return false
     return False
     
 def subdivisions(n, s):
@@ -107,23 +175,23 @@ def subdivisions(n, s):
 def DDM_Practical3(context):
     print ("DDM Practical 3")
     n = 10
-    length = 4
-    s = 3
+    length = 9
+    s = 1
     
     A = control_mesh(n, length)
 
-    show_mesh(mesh_from_array(A, n))
+    #show_mesh(mesh_from_array(A, n))
     B = De_Casteljau(A, n, s)
     
     n_B = subdivisions(n, s)
-    print("n_B size: " + str(n_B))
+    #print("n_B size: " + str(n_B))
     
     show_mesh(mesh_from_array(B, n_B))
     
-    p1 = (1,2,3)
-    p2 = (3,4,5)
+    p1 = (1, 2, 2)
+    p2 = (1, 2, 0)
     
-    print(line_intersect(A, n, p1, p2, 0.01))
+    print("lineIntersect:", line_intersect(B, n_B, p1, p2, 0.1))
     
 # Builds a mesh using a list of triangles
 # This function is the same as the previous practical
